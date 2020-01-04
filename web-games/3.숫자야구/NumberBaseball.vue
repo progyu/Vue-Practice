0fdd67d6a1b8608829979e440f83e74579c1c190
progyu <rocket7101@gmail.com>
2020-01-04 14:00:43 +0900

    add: NumberBaseball
    chore: Setting a webpack
    feat: input two-way data binding, Display try

---
+++ web-games/3.숫자야구/NumberBaseball.vue
@@ -0,0 +1,42 @@
+<template>
+	<div>
+		<h1>{{ result }}</h1>
+		<form @submit.prevent="onSubmitForm">
+			<input ref="answer" minlength="4" maxlength="4" v-model="value" />
+			<button type="submit">입력</button>
+		</form>
+		<div>시도: {{ tries.length }}</div>
+		<ul>
+			<li v-for="t in tries" v-bind:key="t.id">
+        <div>{{t.try}}</div>
+        <div>{{t.result}}</div>
+      </li>
+		</ul>
+	</div>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+      id: 0,
+			tries: [],
+			value: '',
+			result: '',
+		};
+	},
+	methods: {
+		onSubmitForm(e) {
+			this.tries.push({
+        id: this.id++,
+        try: this.value,
+        result: '홈런',
+      });
+			this.value = '';
+			this.$refs.answer.focus();
+		},
+	},
+};
+</script>
+
+<style></style>
---
+++ web-games/3.숫자야구/main.js
@@ -0,0 +1,4 @@
+import Vue from 'vue';
+import NumberBaseball from './NumberBaseball';
+
+new Vue(NumberBaseball).$mount('#root'); // el 역할을 해준다. Vue 인스턴스
