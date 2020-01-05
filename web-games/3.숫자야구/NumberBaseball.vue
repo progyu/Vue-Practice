819a073631db14b71da30f9ce0def42e727435c8
progyu <rocket7101@gmail.com>
2020-01-05 18:16:07 +0900

    feat: numberBaseball
    랜덤으로 4자리의 숫자를 생성하는 로직 추가
    볼인지 스트라이크인지 확인하는 로직 추가
    정답을 맞추거나 10번 이상 틀릴 시 게임을 재시작하는 로직 추가

--- web-games/3.숫자야구/NumberBaseball.vue
+++ web-games/3.숫자야구/NumberBaseball.vue
@@ -8,32 +8,79 @@
 		<div>시도: {{ tries.length }}</div>
 		<ul>
 			<li v-for="t in tries" v-bind:key="t.id">
-        <div>{{t.try}}</div>
-        <div>{{t.result}}</div>
-      </li>
+				<div>{{ t.try }}</div>
+				<div>{{ t.result }}</div>
+			</li>
+			<div>{{ answer }}</div>
+			<div>{{ id }}</div>
 		</ul>
 	</div>
 </template>
 
 <script>
+const getNumbers = () => {
+	const candidates = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+	const array = [];
+	for (let i = 0; i < 4; i += 1) {
+		const chosen = candidates.splice(Math.floor(Math.random() * (9 - i)), 1)[0];
+		array.push(chosen);
+	}
+	return array;
+};
+
 export default {
 	data() {
 		return {
-      id: 0,
+			id: 0,
+			answer: getNumbers(),
 			tries: [],
 			value: '',
 			result: '',
 		};
 	},
 	methods: {
-		onSubmitForm(e) {
-			this.tries.push({
-        id: this.id++,
-        try: this.value,
-        result: '홈런',
-      });
+		reStartGame() {
+			alert('게임을 다시 시작합니다');
+			this.id = 0;
 			this.value = '';
+			this.answer = getNumbers();
+			this.tries = [];
 			this.$refs.answer.focus();
+		},
+		onSubmitForm(e) {
+			if (!this.value.trim()) return;
+			if (this.value === this.answer.join('')) {
+				this.tries.push({
+					id: this.id++,
+					try: this.value,
+					result: '홈런',
+				});
+				this.result = '홈런';
+				this.reStartGame();
+			} else {
+				if (this.tries.length >= 9) {
+					this.result = `실패! 답은 ${this.answer.join(',')}이었습니다.`;
+					this.reStartGame();
+				} else {
+					let strike = 0;
+					let ball = 0;
+					const answerArray = this.value.split('').map(v => parseInt(v));
+					for (let i = 0; i < 4; i += 1) {
+						if (answerArray[i] === this.answer[i]) {
+							strike++;
+						} else if (this.answer.includes(answerArray[i])) {
+							ball++;
+						}
+					}
+					this.tries.push({
+						id: this.id++,
+						try: this.value,
+						result: `${strike} 스트라이크, ${ball} 볼입니다.`,
+					});
+					this.value = '';
+					this.$refs.answer.focus();
+				}
+			}
 		},
 	},
 };
